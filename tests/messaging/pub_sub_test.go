578ee7199ee2391887ff064216974e4b6b367950
Banyc <36535895+Banyc@users.noreply.github.com>
2022-01-01 01:36:10 +0800

    feat: test restart pub

--- tests/messaging/pub_sub_test.go
+++ tests/messaging/pub_sub_test.go
@@ -3,6 +3,7 @@ package messaging_test
 import (
 	"net"
 	"simple-messaging/messaging"
+	"sync"
 	"testing"
 	"time"
 )
@@ -169,4 +170,73 @@ func TestPubSub4(t *testing.T) {
 			t.Fatal("Expected 'def' but got '" + string(subRecvBytes) + "'")
 		}
 	}
+}
+
+func TestPubSub5(t *testing.T) {
+	pubAddr, err := net.ResolveTCPAddr("tcp", "localhost:8080")
+	if err != nil {
+		t.Fatal(err)
+	}
+	subs := make([]*messaging.Subscriber, 0)
+	for i := 0; i < 10; i++ {
+		sub := messaging.NewSubscriber(pubAddr, 1024)
+		sub.Start()
+		defer sub.Close()
+		subs = append(subs, sub)
+	}
+
+	pubListenAddr, err := net.ResolveTCPAddr("tcp", "localhost:8080")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pub := messaging.NewPublisher(pubListenAddr)
+	pub.Start()
+	defer pub.Close()
+
+	time.Sleep(time.Second * 3)
+
+	pub.Send([]byte("Hello"))
+	pub.Send([]byte("World!"))
+	pub.Send([]byte("Hello World!"))
+	for _, sub := range subs {
+		subRecvBytes := sub.EnsureReceived()
+		if string(subRecvBytes) != "Hello" {
+			t.Fatal("Expected 'Hello' but got '" + string(subRecvBytes) + "'")
+		}
+		subRecvBytes = sub.EnsureReceived()
+		if string(subRecvBytes) != "World!" {
+			t.Fatal("Expected 'World!' but got '" + string(subRecvBytes) + "'")
+		}
+		subRecvBytes = sub.EnsureReceived()
+		if string(subRecvBytes) != "Hello World!" {
+			t.Fatal("Expected 'Hello World!' but got '" + string(subRecvBytes) + "'")
+		}
+	}
+
+	wg := sync.WaitGroup{}
+
+	for _, sub := range subs {
+		wg.Add(1)
+		go func(sub *messaging.Subscriber) {
+			subRecvBytes := sub.EnsureReceived()
+			if string(subRecvBytes) != "abc" {
+				t.Fatal("Expected 'abc' but got '" + string(subRecvBytes) + "'")
+			}
+			subRecvBytes = sub.EnsureReceived()
+			if string(subRecvBytes) != "def" {
+				t.Fatal("Expected 'def' but got '" + string(subRecvBytes) + "'")
+			}
+			wg.Done()
+		}(sub)
+	}
+
+	// restart publisher
+	pub.Close()
+	pub = messaging.NewPublisher(pubListenAddr)
+	pub.Start()
+	time.Sleep(time.Second * 3)
+	pub.Send([]byte("abc"))
+	pub.Send([]byte("def"))
+
+	wg.Wait()
 }
